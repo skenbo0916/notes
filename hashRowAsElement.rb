cfa42e273e64d716caeb5cb4cd09ec66ae113ab8
Benny <skenbo0916@gmail.com>
2015-01-08 03:21:29 +0800

    parse .csv as [{}, {}, ..., {}] by Ruby

---
+++ hashRowAsElement.rb
@@ -0,0 +1,26 @@
+def hashRowAsElement ( csvString, keyWord )
+    termMap, res, startTag, temp = {}, [], FALSE, {}
+
+    csvString.each do | eachRow |
+	eachRow = eachRow.split( ',' )
+
+	if startTag
+	    temp.clear
+
+	    eachRow.each_with_index do | element, key |
+		temp[ termMap[key] ] = element
+	    end
+
+	    res.push( temp )
+	elsif eachRow[0].to_s == keyWord
+	    eachRow.each_with_index do | term, key |
+		termMap[key] = term
+	    end
+
+	    startTag = TRUE
+	end
+    end
+
+    puts res.inspect
+
+end
